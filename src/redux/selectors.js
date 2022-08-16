0b696b80a438cfb7934b25bc2dc12460ce00dd6d
Roman Havrylenko <g4vr0m@gmail.com>
2022-08-16 23:21:30 +0300

    fix filter bug

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -6,7 +6,7 @@ export const getFilterValue = state => state.contacts.filter;
 
 export const getFilteredContacts = state => {
     const contacts = getContacts(state);
-    const filter = getFilterValue(state);
+    const filter = getFilterValue(state).toLowerCase();
     return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
 };
 
