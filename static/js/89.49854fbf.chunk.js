63cd544c7b2f72744665766028466814f18fcce5
nap4sov <g4vr0m@gmail.com>
2022-08-17 12:27:21 +0000

    Deploying to gh-pages from @ nap4sov/goit-react-hw-08-phonebook@4b7a580f9c8415be0b449a8bd54afc40e5bd0f28 🚀

---
+++ static/js/89.49854fbf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[89],{3955:function(e,t,o){o.r(t);var n=o(6871),r=o(184);t.default=function(){return(0,r.jsx)(n.Fg,{to:"/register"})}}}]);
+//# sourceMappingURL=89.49854fbf.chunk.js.map
